844beeb34422a528707c5f6d93cbbaeb393f5f05
Konstantin Ananev <russrage@gmail.com>
2025-12-13 23:40:10 +0300

    fix user crud

--- src/crud/user.py
+++ src/crud/user.py
@@ -1,4 +1,6 @@
-from sqlalchemy import or_, select
+from typing import Optional
+
+from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from crud.base import CRUDBase
@@ -7,19 +9,25 @@ from schemas.user import UserCreate, UserUpdate
 
 
 class UserCRUD(CRUDBase[User, UserCreate, UserUpdate]):
-    """CRUD операции для User."""
+    """CRUD операции для модели User."""
 
-    async def get_by_email_or_phone(
+    async def get_by_email(
         self,
-        user: UserCreate,
+        email: str,
         session: AsyncSession,
-    ) -> User | None:
-        """Получает пользователя по email или телефону."""
-        stmt = select(User).where(
-            or_(User.email == user.email, User.phone == user.phone),
-        )
-        result = await session.execute(stmt)
-        return result.scalar_one_or_none()
+    ) -> Optional[User]:
+        """Получает пользователя по адресу электронной почты."""
+        result = await session.execute(select(User).where(User.email == email))
+        return result.scalars().first()
+
+    async def get_by_phone(
+        self,
+        phone: str,
+        session: AsyncSession,
+    ) -> Optional[User]:
+        """Получает пользователя по номеру телефона."""
+        result = await session.execute(select(User).where(User.phone == phone))
+        return result.scalars().first()
 
 
 user_crud = UserCRUD(User)
