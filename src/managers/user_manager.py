7fb1eaa26f3e203c1a14a57db7a12a3c20fe8545
Konstantin Ananev <russrage@gmail.com>
2025-12-14 12:08:16 +0300

    add create roles and superuser

--- src/managers/user_manager.py
+++ src/managers/user_manager.py
@@ -2,7 +2,8 @@ from sqlalchemy.ext.asyncio import AsyncSession
 
 from core.security import get_password_hash
 from crud.user import user_crud
-from schemas.user import UserCreate, UserCreateInternal, UserDB
+from models.user import User, UserRole
+from schemas.user import UserCreate, UserCreateDB
 
 
 class UserManager:
@@ -12,8 +13,13 @@ class UserManager:
         """Инициализирует менеджер сессией базы данных."""
         self.session = session
 
-    async def create_user(self, user: UserCreate) -> UserDB:
-        """Создаёт нового пользователя с проверкой уникальности.
+    async def _create_user(
+        self,
+        user: UserCreate,
+        role: UserRole = UserRole.USER,
+        is_superuser: bool = False,
+    ) -> User:
+        """Приватный метод для создания пользователя.
 
         Проверяются поля email и телефона.
         """
@@ -34,12 +40,40 @@ class UserManager:
                 raise ValueError(f'Пользователь {user.phone} уже существует')
 
         hashed_password = get_password_hash(user.password)
-        user_internal = UserCreateInternal(
+        user_internal = UserCreateDB(
             **user.model_dump(exclude={'password'}),
             hashed_password=hashed_password,
+            role=role,
+            is_superuser=is_superuser,
         )
-        db_user = await user_crud.create(
+
+        return await user_crud.create(
             obj_in=user_internal,
             session=self.session,
         )
-        return UserDB.model_validate(db_user)
+
+    async def create_user(self, user: UserCreate) -> User:
+        """Создаёт обычного пользователя (роль USER)."""
+        return await self._create_user(user=user)
+
+    async def create_manager(
+        self,
+        user: UserCreate,
+    ) -> User:
+        """Создаёт пользователя с ролью MANAGER."""
+        return await self._create_user(user=user, role=UserRole.MANAGER)
+
+    async def create_admin(
+        self,
+        user: UserCreate,
+    ) -> User:
+        """Создаёт пользователя с ролью ADMIN."""
+        return await self._create_user(user=user, role=UserRole.ADMIN)
+
+    async def create_superuser(self, user: UserCreate) -> User:
+        """Создаёт суперпользователя (роль ADMIN + is_superuser=True)."""
+        return await self._create_user(
+            user=user,
+            role=UserRole.ADMIN,
+            is_superuser=True,
+        )
