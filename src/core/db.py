bc6690ff5472dd55300cd09918a0e30f66b821db
Konstantin Ananev <russrage@gmail.com>
2025-12-15 09:52:20 +0300

    class base in sqlalchemy 2.0

--- src/core/db.py
+++ src/core/db.py
@@ -1,40 +1,50 @@
 import uuid
 from typing import AsyncGenerator
 
-from sqlalchemy import Column
 from sqlalchemy.dialects.postgresql import UUID
 from sqlalchemy.ext.asyncio import (
     AsyncEngine,
     AsyncSession,
+    async_sessionmaker,
     create_async_engine,
 )
-from sqlalchemy.orm import declarative_base, declared_attr, sessionmaker
+from sqlalchemy.orm import (
+    DeclarativeBase,
+    Mapped,
+    declared_attr,
+    mapped_column,
+)
 
 from core.config import settings
 
 
-class PreBase:
-    """Базовый класс для моделей SQLAlchemy."""
+class Base(DeclarativeBase):
+    """Базовый класс для всех моделей."""
 
-    id = Column(
+    id: Mapped[uuid.UUID] = mapped_column(
         UUID(as_uuid=True),
         primary_key=True,
         default=uuid.uuid4,
-        nullable=False,
     )
 
-    @declared_attr
-    def __tablename__(self) -> str:
-        return self.__name__.lower()
+    @declared_attr.directive
+    def __tablename__(cls) -> str:  # noqa: N805
+        return cls.__name__.lower()
 
 
-Base = declarative_base(cls=PreBase)
-engine: AsyncEngine = create_async_engine(settings.database_url)
+engine: AsyncEngine = create_async_engine(
+    settings.database_url,
+    echo=False,
+    future=True,
+)
 
-AsyncSessionLocal = sessionmaker(bind=engine, class_=AsyncSession)
+AsyncSessionLocal = async_sessionmaker(
+    engine,
+    expire_on_commit=False,
+)
 
 
 async def get_async_session() -> AsyncGenerator[AsyncSession, None]:
-    """Возвращает асинхронную сессию SQLAlchemy."""
-    async with AsyncSessionLocal() as async_session:
-        yield async_session
+    """Dependency для получения AsyncSession."""
+    async with AsyncSessionLocal() as session:
+        yield session
