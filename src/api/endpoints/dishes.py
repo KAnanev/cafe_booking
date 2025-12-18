2b73f3a60e2c58d7727a68b732358419731d6f6c
Yury Kosulnikov <yv.kosulnikov@arriam.ru>
2025-12-18 14:51:59 +0300

    clean

--- src/api/endpoints/dishes.py
+++ src/api/endpoints/dishes.py
@@ -1,21 +1,22 @@
-from typing import List, Sequence
+from typing import List
 from uuid import UUID
 
 from fastapi import APIRouter, Depends, HTTPException, Query, status
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from api.dependencies.permissions import is_manager_or_admin
+from api.dependencies.users import get_current_active_user
 from api.utils import build_dish_response
 from core.db import get_async_session
 from crud.dishes import dish_crud
-from models.user import User
-from schemas.dishes import DishCreate, DishRead, DishUpdate
 from models.dishes import Dish
+from models.user import User, UserRole
+from schemas.dishes import DishCreate, DishRead, DishUpdate
 
 router = APIRouter()
 
 
-def build_dish_response(dish) -> DishRead:
+def build_dish_response(dish: Dish) -> DishRead:
     """Формирует ответ по блюду согласно схеме DishRead."""
     return DishRead.model_validate(dish, from_attributes=True)
 
@@ -41,13 +42,12 @@ async def get_dishes(
     session: AsyncSession = Depends(get_async_session),
 ) -> List[DishRead]:
     """Получить список блюд, доступных в кафе."""
-    
     dishes = await dish_crud.get_all(
         session=session,
         show_all=show_all,
-        cafe_id=cafe_id
+        cafe_id=cafe_id,
     )
-    
+
     return [build_dish_response(dish) for dish in dishes]
 
 
@@ -62,8 +62,7 @@ async def create_dish(
     session: AsyncSession = Depends(get_async_session),
     _: User = Depends(is_manager_or_admin), # Требуется роль Админ/Менеджер
 ) -> DishRead:
-    """Создать новое блюдо в системе и привязать его к указанным кафе."""
-
+    """Создать новое блюдо в системе."""
     try:
         dish = await dish_crud.create(obj_in=dish_in, session=session)
         return build_dish_response(dish)
@@ -84,7 +83,7 @@ async def get_dish(
     if not dish.is_active:
          is_admin_or_manager = (_.role in (UserRole.MANAGER, UserRole.ADMIN))
          if not is_admin_or_manager:
-             raise HTTPException(status.HTTP_404_NOT_FOUND, 'Блюдо не найдено или не активно.')
+             raise HTTPException(status.HTTP_404_NOT_FOUND, 'Блюдо не найдено')
 
     return build_dish_response(dish)
 
