a4f58da50f52d9257de5993eba5e2d0831f3c3ed
Константин А <russrage@gmail.com>
2026-01-04 23:18:37 +0300

    add outbox_manager

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -7,3 +7,4 @@ from .slots import Slot  # noqa F401
 from .booking import Booking, BookingStatus  # noqa F401
 from .booking_table_slot import BookingTableSlot  # noqa F401
 from .dishes import Dish  # noqa F401
+from .outbox_message import OutboxStatus, OutboxMessage  # noqa F401
