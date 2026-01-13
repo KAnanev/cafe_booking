0fdce168ed3b99b6ed7e92231bb321eece081685
AntonPyth <executor1@gmail.com>
2026-01-14 02:09:23 +0300

    updating api/dependencies/media

--- src/api/dependencies/users.py
+++ src/api/dependencies/users.py
@@ -1,18 +1,27 @@
-from typing import Optional
+from fastapi import Depends
 
-from fastapi import Depends, HTTPException, status
-
-from api.dependencies.auth import get_optional_user
+from api.dependencies.auth import require_auth
 from models import User
 
+# Просто алиас для require_auth, если нужно семантическое имя
+get_current_active_user = require_auth
 
-async def get_current_active_user(
-    user: Optional[User] = Depends(get_optional_user),
-) -> User:
-    """Возвращает аутентифицированного юзера или вызывает 401 ошибку."""
-    if not user:
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            detail='Not authenticated',
-        )
-    return user
+
+# from typing import Optional
+
+# from fastapi import Depends, HTTPException, status
+
+# from api.dependencies.auth import get_optional_user
+# from models import User
+
+
+# async def get_current_active_user(
+#     user: Optional[User] = Depends(get_optional_user),
+# ) -> User:
+#     """Возвращает аутентифицированного юзера или вызывает 401 ошибку."""
+#     if not user:
+#         raise HTTPException(
+#             status_code=status.HTTP_401_UNAUTHORIZED,
+#             detail='Not authenticated',
+#         )
+#     return user
