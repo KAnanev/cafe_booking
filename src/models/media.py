451c33344ae3d9c746e312aee1c605d1eb8f2ae8
AntonPyth <executor1@gmail.com>
2026-01-15 03:35:26 +0300

    fixing error with images

--- src/models/media.py
+++ src/models/media.py
@@ -1,5 +1,8 @@
+import uuid
+
 from sqlalchemy import String
 from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.dialects.postgresql import UUID
 
 from core.db import Base
 from models.mixins import ActiveMixin, TimestampMixin
@@ -8,6 +11,11 @@ from models.mixins import ActiveMixin, TimestampMixin
 class Image(TimestampMixin, ActiveMixin, Base):
     """Модель для хранения метаданных изображений в системы."""
 
+    id: Mapped[uuid.UUID] = mapped_column(
+        UUID(as_uuid=True),
+        primary_key=True,
+        default=uuid.uuid4,
+    )
     file_path: Mapped[str] = mapped_column(
         String(),
         nullable=False,
